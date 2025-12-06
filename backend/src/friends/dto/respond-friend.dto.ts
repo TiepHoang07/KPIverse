1a810d361c56d7ae1ec17b1c979fe56675becf90
hoangv <hoanhv@ssemi.cz>
2025-12-06 21:05:19 +0100

    update friend module

--- backend/src/friends/dto/respond-friend.dto.ts
+++ backend/src/friends/dto/respond-friend.dto.ts
@@ -1,10 +1,14 @@
-import { IsEnum, IsInt } from "class-validator";
-import { FriendStatus } from "generated/prisma/enums";
+import { IsEnum, IsIn, IsInt } from 'class-validator';
+import { FriendStatus } from 'generated/prisma/enums';
 
 export class RespondFriendDto {
   @IsInt()
-  friendId: number
+  friendId: number;
 
-  @IsEnum(FriendStatus)
-  action: FriendStatus;
+  // @IsEnum(FriendStatus)
+  // action: FriendStatus;
+  // @IsIn(Object.values(FriendStatus))
+  // action: FriendStatus;
+  @IsIn(['ACCEPTED', 'BLOCKED'])
+  action: any;
 }
