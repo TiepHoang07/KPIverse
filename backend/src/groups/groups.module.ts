b99b562e66404bb575df59da110634cd5d750a36
hoangv <tieppx0712@gmail.com>
2025-12-11 23:05:07 +0100

    fix bug

--- backend/src/groups/groups.module.ts
+++ backend/src/groups/groups.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { GroupsController } from './groups.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+
 
 @Module({
-  providers: [GroupsService],
+  providers: [GroupsService, PrismaService],
   controllers: [GroupsController]
 })
 export class GroupsModule {}
