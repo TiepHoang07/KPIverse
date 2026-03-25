6e7c25a4fd4567ba758a5f5fd781f9db2ec6c8a0
TiepHoang07 <tieppx0712@gmail.com>
2026-03-25 23:59:44 +0100

    update: prepare app for production deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:5173', // frontend Vite
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
   
